62747de348f5b444c02470e8aad25ceeb9646add
zeeshanbarcha <zeeshanbarcha554@gmail.com>
2024-12-27 13:31:12 +0500

    adding migration

---
+++ prisma/migrations/20241226193013_products/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_ProductToUser` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_ProductToUser" DROP CONSTRAINT "_ProductToUser_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ProductToUser" DROP CONSTRAINT "_ProductToUser_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "featured" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "flashSale" BOOLEAN NOT NULL DEFAULT false;
+
+-- DropTable
+DROP TABLE "_ProductToUser";
+
+-- CreateTable
+CREATE TABLE "_UserWishlist" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserWishlist_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_UserWishlist_B_index" ON "_UserWishlist"("B");
+
+-- AddForeignKey
+ALTER TABLE "_UserWishlist" ADD CONSTRAINT "_UserWishlist_A_fkey" FOREIGN KEY ("A") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserWishlist" ADD CONSTRAINT "_UserWishlist_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
